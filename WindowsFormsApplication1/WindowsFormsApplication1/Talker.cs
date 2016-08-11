3ed77004a5bf68ee3741a831fe3d297f2521f9eb
Gaurav Sathe <gsathe@usc.edu>
2016-08-10 23:45:04 -0700

    First Projects

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Talker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    class Talker
+    {
+        public static int BlahBlahBlah(string thingToSay, int noOfTimes)
+        {
+            string finalString = "";
+            for(int count=0; count<noOfTimes; count++)
+            {
+                finalString += thingToSay + "\n";
+            }
+            MessageBox.Show(finalString);
+
+            return finalString.Length;
+        }
+    }
+}
